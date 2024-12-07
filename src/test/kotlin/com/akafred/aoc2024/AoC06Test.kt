9b61b5b687dbc3338669224a0ba3e8bcbfedada5
akafred <github@akafred.com>
2024-12-07 22:41:11 +0100

    2024-06 part 2 ... not solved.

--- src/test/kotlin/com/akafred/aoc2024/AoC06Test.kt
+++ src/test/kotlin/com/akafred/aoc2024/AoC06Test.kt
@@ -3,22 +3,32 @@ package com.akafred.aoc2024
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-typealias Row = String
+typealias Row = List<Char>
 typealias Board = List<Row>
-typealias Vec = Pair<Int, Int>
 typealias Pos = Pair<Int,Int>
+typealias Vec = Pair<Int,Int>
+typealias Visit = Pair<Pos, Direction>
+typealias Visits = List<Visit>
 typealias Direction = Vec
 typealias Directions = List<Vec>
 
+val directions: Directions = listOf<Direction>(Vec(-1,0), Vec(0,1), Vec(1,0), Vec(0,-1))
+
 fun Pos.inside(board: Board): Boolean =
     0 <= this.first && this.first < board.size
-            && 0 <= this.second && this.second < board[0].length
+            && 0 <= this.second && this.second < board[0].size
 
 fun Pos.move(vec: Vec) =
     Pair(this.first + vec.first, this.second + vec.second)
 
 fun Board.get(pos:Pos): Char =
     this[pos.first][pos.second]
+
+fun Board.startPos(): Pos = Pair(
+    this.indexOfFirst { row -> row.any { ch -> ch !in setOf('#', '.') } },
+    this.first { row -> row.any { ch -> ch !in setOf('#', '.') } }.indexOfFirst { ch -> ch !in setOf('#', '.') }
+)
+
 
 fun Direction.nextFrom(directions: Directions): Direction =
     directions[(directions.indexOfFirst { it -> it == this } + 1) % directions.size]
@@ -28,8 +38,8 @@ class AoC06Test {
     private val inputFile = "input06.txt"
     private val example1Answer = 41
     private val puzzle1Answer = 4982
-    private val example2Answer = -1
-    private val puzzle2Answer = -1
+    private val example2Answer = 6
+    private val puzzle2Answer = -1 // ikke 1753 ...
 
     private val exampleInput1 = """
                 ....#.....
@@ -47,7 +57,7 @@ class AoC06Test {
     private val exampleInput2 = exampleInput1
 
     private fun solve1(input: String): Int {
-        val board: Board = input.lines()
+        val board: Board = input.lines().map { line -> line.toList() }
         var visited: Set<Pos> = setOf()
         var guardPos: Pos = Pair(
             board.indexOfFirst { row -> row.any { ch -> ch !in setOf('#', '.') } },
@@ -56,7 +66,7 @@ class AoC06Test {
         val directions = listOf<Vec>(Vec(-1,0), Vec(0,1), Vec(1,0), Vec(0,-1))
         var guardDirection = if (board.get(guardPos) == '^') Vec(-1,0) else throw IllegalArgumentException("I only handle '^' guards!")
         while(guardPos.inside(board)) {
-            visited = visited + guardPos
+            visited += guardPos
             var candidateMove = guardPos.move(guardDirection)
             if (candidateMove.inside(board)) {
                 while (board.get(candidateMove) == '#') {
@@ -69,8 +79,75 @@ class AoC06Test {
         return visited.size
     }
 
+
     private fun solve2(input: String): Int {
-        TODO("Not yet implemented")
+        val board: Board = input.lines().map { line -> line.toList() }
+        var visits: Visits = listOf()
+        var guardPos: Pos = board.startPos()
+        var placedObstacles: List<Pos> = listOf()
+        var guardDirection = if (board.get(guardPos) == '^') Vec(-1,0) else throw IllegalArgumentException("I only handle '^' guards!")
+        while(guardPos.inside(board)) {
+            visits += Visit(guardPos, guardDirection)
+            var candidateMove = guardPos.move(guardDirection)
+            while (candidateMove.inside(board) && board.get(candidateMove) == '#') {
+                guardDirection = guardDirection.nextFrom(directions)
+                candidateMove = guardPos.move(guardDirection)
+            }
+            val potentialObstacle = candidateMove
+            if (potentialObstacle.inside(board) && potentialObstacle != board.startPos()) {
+                var potentialGuardDirection = guardDirection.nextFrom(directions)
+                var potentialGuardPos = guardPos.move(potentialGuardDirection)
+                while(board.get(potentialGuardPos) == '#' || potentialGuardPos == potentialObstacle) {
+                    potentialGuardDirection = potentialGuardDirection.nextFrom(directions)
+                    potentialGuardPos = guardPos.move(potentialGuardDirection)
+                }
+                var walked: Visits = listOf()
+                var candidatePotentialGuardPos = potentialGuardPos
+                var looped = false
+                while (candidatePotentialGuardPos.inside(board) && !looped) {
+                    while(board.get(candidatePotentialGuardPos) == '#' || candidatePotentialGuardPos == potentialObstacle) {
+                        potentialGuardDirection = potentialGuardDirection.nextFrom(directions)
+                        candidatePotentialGuardPos = potentialGuardPos.move(potentialGuardDirection)
+                    }
+                    potentialGuardPos = candidatePotentialGuardPos
+                    if (visits.contains(Visit(potentialGuardPos, potentialGuardDirection))
+                        || walked.contains(Visit(potentialGuardPos, potentialGuardDirection))) {
+                        looped = true
+                    } else {
+                        walked += Visit(potentialGuardPos, potentialGuardDirection)
+                        candidatePotentialGuardPos = potentialGuardPos.move(potentialGuardDirection)
+                    }
+                }
+                if (looped) {
+                    placedObstacles += potentialObstacle
+                    //draw(board, visits, walked, placedObstacles)
+                }
+            }
+            guardPos = candidateMove
+        }
+        return placedObstacles.toSet().size
+    }
+
+    private fun draw(board: Board, visits: Visits, walked: Visits, obstacles: List<Pos>) {
+        print("\u001b[H\u001b[2J")
+        System.out.flush()
+        val mutableBoard = board.map { line -> line.toMutableList() }
+        visits.forEach { step ->
+            val (pos, _) = step
+            mutableBoard[pos.first].set(pos.second, '*')
+        }
+        walked.forEach { step ->
+            val (pos, _) = step
+            mutableBoard[pos.first].set(pos.second, '+')
+        }
+        obstacles.forEach { pos ->
+            mutableBoard[pos.first].set(pos.second, 'O')
+        }
+        mutableBoard.forEach { line ->
+            line.forEach { ch -> print(ch) }
+            println()
+        }
+        println()
     }
 
     @Test

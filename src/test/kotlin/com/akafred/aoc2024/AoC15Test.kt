160350b74098c4d618670e2b817812e4465debbd
akafred <github@akafred.com>
2024-12-15 14:11:30 +0100

    2024-15 setup

---
+++ src/test/kotlin/com/akafred/aoc2024/AoC15Test.kt
@@ -0,0 +1,93 @@
+package com.akafred.aoc2024
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class AoC15Test {
+
+    private val inputFile = "input15.txt"
+    private val example0Answer = 2028
+    private val example1Answer = 10092
+    private val puzzle1Answer = -1
+    private val example2Answer = -1
+    private val puzzle2Answer = -1
+
+    private val exampleInput0 = """
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+
+        <^^>>>vv<v>>v<<
+    """.trimIndent()
+
+    private val exampleInput1 = """
+        ##########
+        #..O..O.O#
+        #......O.#
+        #.OO..O.O#
+        #..O@..O.#
+        #O#..O...#
+        #O..O..O.#
+        #.OO.O.OO#
+        #....O...#
+        ##########
+
+        <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+        vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+        ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+        <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+        ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+        ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+        >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+        <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+        ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+        v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+    """.trimIndent()
+
+    private val exampleInput2 = exampleInput1
+
+    private fun solve1(input: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    private fun solve2(input: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `example 0`() {
+        val result = solve1(exampleInput0)
+        assertEquals(example0Answer, result)
+    }
+
+    @Test
+    fun `example 1`() {
+        val result = solve1(exampleInput1)
+        assertEquals(example1Answer, result)
+    }
+
+    @Test
+    fun `puzzle 1`() {
+        val input = Util.readFile(inputFile)
+        val result = solve1(input)
+        assertEquals(puzzle1Answer, result)
+    }
+
+    @Test
+    fun `example 2`() {
+        val result = solve2(exampleInput2)
+        assertEquals(example2Answer, result)
+    }
+
+    @Test
+    fun `puzzle 2`() {
+        val input = Util.readFile(inputFile)
+        val result = solve2(input)
+        assertEquals(puzzle2Answer, result)
+    }
+}

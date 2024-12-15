3b0e88ef9f8b141aa5ed4de4070ed700d966b872
akafred <github@akafred.com>
2024-12-15 19:07:51 +0100

    2024-14 part 2 fail!

--- src/test/kotlin/com/akafred/aoc2024/AoC14Test.kt
+++ src/test/kotlin/com/akafred/aoc2024/AoC14Test.kt
@@ -9,7 +9,7 @@ class AoC14Test {
     private val example1Answer = 12
     private val puzzle1Answer = 221142636
     private val example2Answer = -1
-    private val puzzle2Answer = -1
+    private val puzzle2Answer = 221142636
 
     private val exampleInput1 = """
         p=0,4 v=3,-3
@@ -46,8 +46,49 @@ class AoC14Test {
         return countPerQuadrant.fold(1) { acc, pos -> acc * pos }
     }
 
-    private fun solve2(input: String): Int {
-        TODO("Not yet implemented")
+    private fun solve2(input: String, width: Int, height: Int): Int {
+        val startPos =
+            input.lines()
+                .map { line -> """(-?\d+)""".toRegex().findAll(line).map { it.groupValues[1].toInt() }.toList() }
+                .map { robotValues -> Pair(Pos(robotValues[0], robotValues[1]), Vec(robotValues[2], robotValues[3])) }
+        print(height, width, startPos)
+        var pos = startPos
+        (1..100).forEach { i ->
+            pos = pos.map { (pos, vec) -> Pair(Pos((pos.first + vec.first + width) % width, (pos.second + vec.second + height) % height), vec)}
+            if (true) { // (0 ..height - 1).count { i -> pos.count { (pos, _) -> pos.second == i } > 15 } > 10) {
+                println("**** AFTER $i seconds ****")
+                print(height, width, pos)
+                println()
+            }
+        }
+
+        val countPerQuadrant: List<Int> =
+            pos.map { it.first }.fold(mutableListOf<Int>(0,0,0,0)) { acc, pos ->
+                if (pos.first < width / 2 && pos.second < height / 2) acc[0] += 1
+                if (pos.first > width / 2 && pos.second < height / 2) acc[1] += 1
+                if (pos.first < width / 2 && pos.second > height / 2) acc[2] += 1
+                if (pos.first > width / 2 && pos.second > height / 2) acc[3] += 1
+                acc
+            }
+        return countPerQuadrant.fold(1) { acc, pos -> acc * pos }
+    }
+
+    private fun print(
+        height: Int,
+        width: Int,
+        startPos: List<Pair<Pos, Vec>>
+    ) {
+        val mutableBoard = (1..height).map { arrayOfNulls<Int>(width).toMutableList().toTypedArray() }.toTypedArray()
+        startPos.forEach { (pos: Pos, vec: Vec) ->
+            mutableBoard[pos.second][pos.first] =
+                if (mutableBoard[pos.second][pos.first] == null) 1 else mutableBoard[pos.second][pos.first]!! + 1
+        }
+        mutableBoard.forEach { line ->
+            println(line.map {
+                when (it) {
+                    null -> '.'; else -> it.toString(); }
+            }.joinToString(""))
+        }
     }
 
     @Test
@@ -64,15 +105,9 @@ class AoC14Test {
     }
 
     @Test
-    fun `example 2`() {
-        val result = solve2(exampleInput2)
-        assertEquals(example2Answer, result)
-    }
-
-    @Test
     fun `puzzle 2`() {
         val input = Util.readFile(inputFile)
-        val result = solve2(input)
+        val result = solve2(input,101,103)
         assertEquals(puzzle2Answer, result)
     }
 }

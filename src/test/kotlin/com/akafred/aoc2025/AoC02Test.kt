31946bf7c296f06d023b2febe5c32428ff8e94da
akafred <github@akafred.com>
2025-12-06 16:23:24 +0100

    missing expected value 06 Part 2

--- src/test/kotlin/com/akafred/aoc2025/AoC02Test.kt
+++ src/test/kotlin/com/akafred/aoc2025/AoC02Test.kt
@@ -9,7 +9,7 @@ class AoC02Test {
     private val example1Answer = 1227775554L
     private val puzzle1Answer = 18595663903L
     private val example2Answer = 4174379265L
-    private val puzzle2Answer = -1
+    private val puzzle2Answer = 19058204438L
 
     private val exampleInput1 = """
     11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124        
